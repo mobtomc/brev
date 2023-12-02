80f7f4b1441970e2ebf647f4b0eddf2af26ce370
Yugank Bansal <99734769+yugank01@users.noreply.github.com>
2023-12-02 11:54:21 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Logo from "./assets/images/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-3xl font-bold text-red-500'>hello world</p>
+      <img src={Logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
